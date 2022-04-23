1754378afa0affb198875cd859972f2c133914c8
cesarsiguencia <cesar.siguencia@gmail.com>
2022-04-23 14:48:57 -0400

    added comment routes and controller

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
+const commentRoutes = require('./comment-routes') // this has to be before the pizza routes
 const pizzaRoutes = require('./pizza-routes');
 
+
 // add prefix of `/pizzas` to routes created in `pizza-routes.js`
+router.use('/comments', commentRoutes) //this has to be before the pizza routes
 router.use('/pizzas', pizzaRoutes);
+
 
 module.exports = router;
